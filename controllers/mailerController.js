a8c483ee3e5482541aa93606c98bebcbe5574557
agungd3v <mailtotuyul1@gmail.com>
2021-08-22 23:38:14 +0700

    dynamic message for blast email

--- controllers/mailerController.js
+++ controllers/mailerController.js
@@ -5,7 +5,6 @@ const mailerController = {
     const { authorization, from, to, subject, message } = req.body
     const transporter = await mailer(authorization.host, authorization.user, authorization.pass)
     transporter.verify((err) => {
-      let parseEmailToStr
       if (err) {
         return res.json({
           status: false,
@@ -19,15 +18,16 @@ const mailerController = {
         } else if (!subject) {
           return res.json({ status: false, message: 'Please enter subject for message' })
         } else {
-          to.length > 1 ? parseEmailToStr = to.join(', ') : parseEmailToStr = to.toString()
-          transporter.sendMail({
-            from: `"${from}" <${authorization.user}>`,
-            to: parseEmailToStr,
-            subject: subject,
-            html: `<h1>${message}</h1>`
-          }, err => {
-            if (err) return res.json({ status: false, message: 'Sending blast failed' })
-            return res.json({ status: true, message: 'Sending blast complete' })
+          to.map(data => {
+            transporter.sendMail({
+              from: `"${from}" <${authorization.user}>`,
+              to: data,
+              subject: subject,
+              html: message
+            }, err => {
+              if (err) return res.json({ status: false, message: 'Sending blast failed' })
+              return res.json({ status: true, message: 'Sending blast complete' })
+            })
           })
         }
       }
